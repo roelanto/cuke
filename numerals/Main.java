20e9cd04de9588b36f84d1d12ee8fe731b04f1cc
Jasperra <jrrademaker@gmail.com>
2017-03-21 13:51:39 +0100

    Added feauture that allows you to get the input squared

--- numerals/Main.java
+++ numerals/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("een");
+
+        String input = args[0];
+        if(input.contains("^")){
+            String[] numbers = input.split("^");
+            int a = Integer.parseInt(numbers[0]);
+
+            System.out.print(a * a);
+        }
     }
 }
